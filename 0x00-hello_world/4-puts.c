dbf68f7abd0254d80bcfdfc3fdccc6e23f26a224
iheb mejri <ihebmejri14@gmail.com>
2019-09-18 13:07:06 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * 
  *main - Start project
- * Return: Alzqys 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }

833ed0f81aa32ff12e24b91e9862d6b4d528d10d
iheb mejri <ihebmejri14@gmail.com>
2019-09-18 12:41:47 +0100

    hello_C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+    #include <stdio.h>
+    int main()
+    {
+      printf("Size of char: %lu ", sizeof(char));
+      printf("Size of char: %lu ", sizeof(int));
+      printf("Size of char: %lu ", sizeof(long));
+      printf("Size of char: %lu ", sizeof(long long));
+      printf("Size of char: %lu ", sizeof(float));
+       return 0;
+    }

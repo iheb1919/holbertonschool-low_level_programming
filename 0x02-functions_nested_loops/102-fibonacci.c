1920a3d4cb55808fa88a9a7a0f2074d4042dce9d
iheb mejri <ihebmejri14@gmail.com>
2019-09-24 16:04:49 +0100

    ddfdfd

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - print the first 50 Fibonacci numbers, starting with 1 and 2. \n
  *
@@ -6,17 +8,21 @@
  */
 int main(void)
 {
-unsigned long int i, j, tmp, n;
-i = 1;
-fibo = 2;
-printf("%lu", i);
-for (n = 1; n < 50; n++)
-{
-printf(", %lu", fibo);
-tmp = i;
-i = fibo;
-fibo += tmp;
-}
-putchar('\n');
-return (0);
+	unsigned long int i, j, tmp, n;
+
+	i = 1;
+	j = 2;
+
+	printf("%lu", i);
+
+	for (n = 1; n < 50; n++)
+	{
+		printf(", %lu", j);
+		tmp = i;
+		i = j;
+		j += tmp;
+	}
+	putchar('\n');
+
+	return (0);
 }

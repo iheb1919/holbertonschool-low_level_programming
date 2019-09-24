7176d928398d89c4204d55891543d3133826a34d
iheb mejri <ihebmejri14@gmail.com>
2019-09-24 14:21:14 +0100

    dd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,9 @@
 void jack_bauer(void)
 {
 int h, m;
-for (m = 0; m < 24; m++)
-{
 for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
 {
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
@@ -19,5 +19,4 @@ _putchar((m % 10) + '0');
 _putchar('\n');
 }
 }
-return (0);
 }

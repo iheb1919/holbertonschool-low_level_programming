c7bab1ccf13a247e5d55ebb546fe84bdf87a6f9e
iheb mejri <ihebmejri14@gmail.com>
2019-09-24 17:50:45 +0100

    fdfdf

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
-
 #include "holberton.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -8,9 +6,9 @@
 
 int main(void)
 {
-int i;
+int i=0;
 
-i = 0;
+
 positive_or_negative(i);
 
 return (0);

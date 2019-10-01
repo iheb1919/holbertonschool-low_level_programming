ea9d02611b530b9a0b96f5c1c066d524351ae2e9
iheb mejri <ihebmejri14@gmail.com>
2019-10-01 14:05:35 +0100

    fklfkfklfklf

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,19 @@
  */
 void print_rev(char *s)
 {
-int i = 0;
-while (*(s + i) != '\0')
-{
-i++;
-}
-while (i > 0)
-{
-_putchar(*(s + i -1));
-i--;
-}
-_putchar('\n');
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	while (i > 0)
+	{
+		_putchar(*(s + i - 1));
+		i--;
+
+	}
+	_putchar('\n');
 }

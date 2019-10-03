84dc59db1d74a1e7af0277e5846c630e4a2be7f3
iheb mejri <ihebmejri14@gmail.com>
2019-10-03 23:25:06 +0100

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,23 @@
 #include "holberton.h"
+
 /**
- * *_srtncat - function that concatinates two strings
- * @*dest: function input
- * @*src: function input
- * @*n: function input
- * Return: A pointer to result
+ * _strncat - Concatenates two strings
+ *
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, l = 0;
-while (dest[i++])
-l++;
-for (i = 0; src[i] && i < n; i++)
-dest[l++] = src[i];
-return (dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }

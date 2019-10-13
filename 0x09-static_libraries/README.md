864e667ee4d16f5e4706993d34fc58c4758d782e
iheb mejri <ihebmejri14@gmail.com>
2019-10-13 01:49:24 +0100

    ff

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
-readme static libraries
+just readme file 
+
--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,5 +1,5 @@
-#ifndef	_HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,6 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 
 #endif

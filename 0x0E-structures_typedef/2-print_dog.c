638f996951d1a8722c9c67b3bbe06fb19f1bd2b4
iheb mejri <ihebmejri14@gmail.com>
2019-10-21 23:19:39 +0100

    error_fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,5 +13,5 @@ d->name = "(nil)";
 if (!(d->owner))
 d->owner = "(nil)";
 }
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }

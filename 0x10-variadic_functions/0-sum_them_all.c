3fc874070e50df03ef8d519fd6caf72168f2f646
iheb mejri <ihebmejri14@gmail.com>
2019-10-27 18:22:10 +0100

    ff

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,16 +3,16 @@
  * sum_them_all - sums a variable number of parameters
  * @n: const unsigned int number of parameters
  * Return: integer sum of int arguments, or 0 is n == 0
- * @...: elipsis
- */
+ * @...: elipsis 
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
 int sum;
 va_list nums;
+va_start(nums, n);
 if (n == 0)
 return (0);
-va_start(nums, n);
 sum = 0;
 for (i = 0; i < n; i++)
 sum += va_arg(nums, int);

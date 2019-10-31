9ce5a7f8301eecf198bd08df0b3b799adda7702b
iheb mejri <ihebmejri14@gmail.com>
2019-10-31 23:42:45 +0100

    dd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,29 @@
+
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to const list_t linked list
- * Return: i  number of nodes in h
+ * print_list - prints all elements of a list_t list
+ * @h: list_t list
+ *
+ * Return: number of nodes in h
  */
+
 size_t print_list(const list_t *h)
 {
-size_t i = 0;
-while (h)
-{
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-i++;
-}
-return (i);
-}
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		count++;
+		h = h->next;
+	}
+
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  *
  */
 
-__attribute__((constructor)) void before_main(void)
+void __attribute__((constructor)) before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");

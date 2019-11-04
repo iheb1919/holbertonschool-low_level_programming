71c8f09ec5aabb59f22d47c78f0904d945a9e624
iheb mejri <ihebmejri14@gmail.com>
2019-11-05 00:20:00 +0100

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,3 @@
-
 #include "lists.h"
 
 /**
@@ -10,22 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
-
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%u] %s\n", h->len, h->str);
-
-		i++;
-		h = h->next;
-	}
-
-	return (i);
+size_t i = 0;
+if (h == NULL)
+return (0);
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+i++;
+h = h->next;
+}
+return (i);
 }
--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,21 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 size_t i = 0;
-
-	if (h == NULL)
-		return (0);
-
-	while (h)
-	{
-		if (h->n)
-		  printf("%d\n", h->n);
-
-		else
-		  printf("0\n");
-
-		i++;
-		h = h->next;
-	}
-
-	return (i);
+if (h == NULL)
+return (0);
+while (h)
+{
+printf("%d\n", h->n);
+i++;
+h = h->next;
+}
+return (i);
 }
